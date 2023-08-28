3533f7f2522be71b29bb14db37415aeed09b3cc6
RaymondAfan <raymond.afan@icloud.com>
2023-08-28 14:54:20 -0700

    update

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,28 +1,34 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
+function setEventListeners(formElement, options){
+  const {inputSelector}= options;
+  const inputElements= [...formElement.querySelectorAll(inputSelector)];
+
+}
 function enableValidation(options) {
-  const formElements= [...document.querySelectorAll(".modal__form")];
-  formElements.forEach((formElements)=>{
-formElements.addEventListener("submit", (e)=>{
+  const formElements= [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formElement)=>{
+formElement.addEventListener("submit", (e)=>{
   e.preventDefault();
 });
-//look for all inputs inside of the form
-//loop thru all inputs to see if all are valid
-//if input isnt valid
-//we need to grab validation message
-//add error class to input
-//display error message
+setEventListeners(formElement, options);
+// look for all inputs inside of the form
+// loop thru all inputs to see if all are valid
+// if input isnt valid
+// we need to grab validation message
+// add error class to input
+// display error message
 // disable button
-//if all inputs valid
-//enable button
-//reset error messages 
+// if all inputs valid
+// then enable button
+// and reset error messages
   });
 }
 
 const config= {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
+  formSelector: ".modal__form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".modal__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible"
