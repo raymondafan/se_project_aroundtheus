e54150053743ac998e06698dc60c8f0888d23f64
RaymondAfan <raymond.afan@icloud.com>
2023-08-27 22:53:42 -0700

    update

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,11 +1,31 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
+function enableValidation(options) {
+  const formElements= [...document.querySelectorAll(".modal__form")];
+  formElements.forEach((formElements)=>{
+formElements.addEventListener("submit", (e)=>{
+  e.preventDefault();
+});
+//look for all inputs inside of the form
+//loop thru all inputs to see if all are valid
+//if input isnt valid
+//we need to grab validation message
+//add error class to input
+//display error message
+// disable button
+//if all inputs valid
+//enable button
+//reset error messages 
+  });
+}
 
-enableValidation({
+const config= {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible"
-});
+};
+
+enableValidation(config);
