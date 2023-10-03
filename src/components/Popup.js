81dbc8edafc207df01e8b2fc8156c08a72e95d56
RaymondAfan <raymond.afan@icloud.com>
2023-10-02 20:37:29 -0700

    adding js classes

---
+++ src/components/Popup.js
@@ -0,0 +1,17 @@
+export default class Popup {
+  constructor({popupSelector}){
+    this._popupElement = document.querySelector(popupSelector);
+  }
+  open(){
+    //opens popup
+  }
+  close(){
+    //closees popup
+  }
+  _handleEscClose(){
+//listens for esc button
+  }
+  setEventListeners(){
+// sets event listeners
+  }
+}
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,18 @@
+import Popup from "./Popup.js";
+class PopupWithForm extends Popup {
+constructor(popupSelector, handleFormSubmit){
+super({popupSelector});
+  this._handleFormSubmit= handleFormSubmit;
+  this._popupForm= this._popupElement.querySelector(".modal__form")
+}
+close(){
+this._popupForm.reset();
+super.close();
+}
+}
+
+
+//index.js
+const newCardPopup = new PopupWithForm("#add-card-modal", ()=>{});
+newCardPopup.open()
+newCardPopup.close();
