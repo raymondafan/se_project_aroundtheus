d55eec06387022e2264f5a82cd5d0116862d89aa
RaymondAfan <raymond.afan@icloud.com>
2023-10-02 22:02:42 -0700

    update

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,17 @@
 import Popup from "./Popup.js";
 class PopupWithForm extends Popup {
-constructor(popupSelector, handleFormSubmit){
-super({popupSelector});
-  this._handleFormSubmit= handleFormSubmit;
-  this._popupForm= this._popupElement.querySelector(".modal__form")
+  constructor(popupSelector, handleFormSubmit) {
+    super({ popupSelector });
+    this._handleFormSubmit = handleFormSubmit;
+    this._popupForm = this._popupElement.querySelector(".modal__form");
+  }
+  close() {
+    this._popupForm.reset();
+    super.close();
+  }
 }
-close(){
-this._popupForm.reset();
-super.close();
-}
-}
-
 
 //index.js
-const newCardPopup = new PopupWithForm("#add-card-modal", ()=>{});
-newCardPopup.open()
+const newCardPopup = new PopupWithForm("#add-card-modal", () => {});
+newCardPopup.open();
 newCardPopup.close();
