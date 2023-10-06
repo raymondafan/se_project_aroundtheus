56068879b87aa614f8fbe59a23dc79ec89f5d969
RaymondAfan <raymond.afan@icloud.com>
2023-10-05 17:47:40 -0700

    update

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ export default class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super({ popupSelector });
     this._handleFormSubmit = handleFormSubmit;
-    this._popupForm = this._popupElement.querySelector("#add-card-preview");
+    this._popupForm = this._popupElement.querySelector(".modal__form");
   }
   close() {
     this._popupForm.reset();
