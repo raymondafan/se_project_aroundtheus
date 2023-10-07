3981d18027248c6a9065a82549264fb72c8dc600
RaymondAfan <raymond.afan@icloud.com>
2023-10-06 17:56:55 -0700

    update

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,10 +1,10 @@
 export default class Section{
   constructor({renderer, selector}){
 this._renderer= renderer;
-this._selecter= document.querySelector(selector);
+this._selector= document.querySelector(selector);
   }
 
-  renderItems(data){
+  renderItems(items){
 //use this._renderer to create elements for rendering
   }
 
